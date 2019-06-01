eaffce0f94b6d92e1bef6869380aeef7eb9f12d3
Andrew Palmer <atp@sdf.org>
2019-06-01 01:37:19 -0500

    Handle DealerHand separately

--- bj.cc
+++ bj.cc
@@ -46,18 +46,19 @@ public:
 
 
 class Hand {
-private:
+protected:
     std::vector<Card> cards_;
 
     Hand(std::vector<Card> cards) {
         cards_ = cards;
     }
 public:
-    static Hand init_from(Shoe &s) {
+    template<class T>
+    static T init_from(Shoe &s) {
         std::vector<Card> cards;
         cards.push_back(s.draw_card());
         cards.push_back(s.draw_card());
-        return Hand(cards);
+        return T(cards);
     }
 
     void add(Card card) {
@@ -67,6 +68,7 @@ public:
     friend std::ostream &operator <<(std::ostream &s, Hand &h);
 };
 
+
 std::ostream &operator <<(std::ostream &s, Hand &h) {
     for(Card c : h.cards_) {
         s << c << ' ';
@@ -75,9 +77,31 @@ std::ostream &operator <<(std::ostream &s, Hand &h) {
 }
 
 
+class DealerHand : public Hand {
+public:
+    DealerHand(std::vector<Card> cards) : Hand(cards) {
+    }
+
+    friend std::ostream &operator <<(std::ostream &s, DealerHand &h);
+};
+
+
+std::ostream &operator <<(std::ostream &s, DealerHand &h) {
+    s << "🂠 " << ' ';
+    std::vector<Card>::iterator ptr = h.cards_.begin();
+    ++ptr;
+    for(;ptr < h.cards_.end(); ++ptr) {
+        s << *ptr << ' ';
+    }
+    return s;
+}
+
+
 int main() {
     Shoe shoe = Shoe::decks(4);
-    Hand hand = Hand::init_from(shoe);
-    std::cout << hand << std::endl;
+    Hand player_hand = Hand::init_from <Hand> (shoe);
+    DealerHand dealer_hand = Hand::init_from <DealerHand> (shoe);
+    std::cout << player_hand << std::endl;
+    std::cout << dealer_hand << std::endl;
     return 0;
 }
