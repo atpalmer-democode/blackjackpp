5b792a8da3ed01e4c69b14045257c803cb82c4f5
Andrew Palmer <atp@sdf.org>
2019-06-01 13:03:12 -0500

    Label player and dealer hands

--- bj.cc
+++ bj.cc
@@ -77,7 +77,7 @@ int main() {
     Shoe shoe = Shoe::decks(4);
     Hand player_hand = Hand::init_from <Hand> (shoe);
     DealerHand dealer_hand = Hand::init_from <DealerHand> (shoe);
-    std::cout << player_hand << " (Value:" << player_hand.value() << ")" << std::endl;
-    std::cout << dealer_hand << std::endl;
+    std::cout << "Player: " << player_hand << " (Value:" << player_hand.value() << ")" << std::endl;
+    std::cout << "Dealer: " << dealer_hand << std::endl;
     return 0;
 }
