2e0d57868ff327a02d72f899d6ee7b958985b585
Andrew Palmer <atp@sdf.org>
2019-06-01 13:49:51 -0500

    Play a turn with hit and stand options

--- bj.cc
+++ bj.cc
@@ -5,11 +5,43 @@
 #include "hand.h"
 
 
+void play_turn(Hand player_hand, DealerHand dealer_hand, Shoe shoe) {
+    if(player_hand.value() == 21) {
+        std::cout << "BLACKJACK!" << std::endl;
+        return;
+    }
+
+    for(;;) {
+        std::cout << "Player: " << player_hand << " (Value:" << player_hand.value() << ")" << std::endl;
+        std::cout << "Dealer: " << dealer_hand << std::endl;
+
+        if(player_hand.value() > 21) {
+            std::cout << "BUSTED!" << std::endl;
+            break;
+        }
+
+        std::string choice;
+        std::cout << "<H>it or <S>tand? ";
+        std::cin >> choice;
+
+        if(std::tolower(choice.front()) == 'h') {
+            player_hand.add(shoe.draw_card());
+            continue;
+        }
+
+        if(std::tolower(choice.front()) == 's') {
+            break;
+        }
+    }
+}
+
+
 int main() {
     Shoe shoe = Shoe::decks(4);
     Hand player_hand = Hand::init_from <Hand> (shoe);
     DealerHand dealer_hand = Hand::init_from <DealerHand> (shoe);
-    std::cout << "Player: " << player_hand << " (Value:" << player_hand.value() << ")" << std::endl;
-    std::cout << "Dealer: " << dealer_hand << std::endl;
+
+    play_turn(player_hand, dealer_hand, shoe);
+
     return 0;
 }
