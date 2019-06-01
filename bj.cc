949e2a0ee4b2f35a8e1d32eab8766704de75ce2c
Andrew Palmer <atp@sdf.org>
2019-06-01 00:53:01 -0500

    Add Hand class

--- bj.cc
+++ bj.cc
@@ -45,8 +45,39 @@ public:
 };
 
 
+class Hand {
+private:
+    std::vector<Card> cards_;
+
+    Hand(std::vector<Card> cards) {
+        cards_ = cards;
+    }
+public:
+    static Hand init_from(Shoe &s) {
+        std::vector<Card> cards;
+        cards.push_back(s.draw_card());
+        cards.push_back(s.draw_card());
+        return Hand(cards);
+    }
+
+    void add(Card card) {
+        cards_.push_back(card);
+    }
+
+    friend std::ostream &operator <<(std::ostream &s, Hand &h);
+};
+
+std::ostream &operator <<(std::ostream &s, Hand &h) {
+    for(Card c : h.cards_) {
+        s << c << ' ';
+    }
+    return s;
+}
+
+
 int main() {
-    Shoe s = Shoe::decks(4);
-    std::cout << s.draw_card() << std::endl;
+    Shoe shoe = Shoe::decks(4);
+    Hand hand = Hand::init_from(shoe);
+    std::cout << hand << std::endl;
     return 0;
 }
