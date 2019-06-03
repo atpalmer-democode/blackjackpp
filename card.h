ac8eec3a13f00aad07d557bfd255823bea5a667c
Andy Palmer <atp@sdf.org>
2019-06-03 16:38:24 -0500

    Remove unused suit and rank definitions

--- card.h
+++ card.h
@@ -12,24 +12,10 @@ private:
     CardRank(std::string name, int value);
 
 public:
-    std::string get_name() const { return name_; }
-    int get_value() const { return value_; }
-
     static const std::array <CardRank, 13> RANKS;
 
-    static const CardRank RA;
-    static const CardRank R2;
-    static const CardRank R3;
-    static const CardRank R4;
-    static const CardRank R5;
-    static const CardRank R6;
-    static const CardRank R7;
-    static const CardRank R8;
-    static const CardRank R9;
-    static const CardRank RT;
-    static const CardRank RJ;
-    static const CardRank RQ;
-    static const CardRank RK;
+    std::string get_name() const { return name_; }
+    int get_value() const { return value_; }
 
     bool operator ==(const CardRank &other) const {
         return this->name_ == other.name_;
@@ -45,15 +31,10 @@ private:
     CardSuit(std::string name, std::string symbol);
 
 public:
-    std::string get_name() const { return name_; }
-    std::string get_symbol() const { return symbol_; }
-
     static const std::array<CardSuit, 4> SUITS;
 
-    static const CardSuit Spades;
-    static const CardSuit Hearts;
-    static const CardSuit Diamonds;
-    static const CardSuit Clubs;
+    std::string get_name() const { return name_; }
+    std::string get_symbol() const { return symbol_; }
 };
 
 
